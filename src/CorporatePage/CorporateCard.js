02a1e909c357bccf0a2c792de5869c33ec0351c4
pragya jain <pragyajain0418@gmail.com>
2024-02-12 17:40:58 +0530

    add changes

--- src/CorporatePage/CorporateCard.js
+++ src/CorporatePage/CorporateCard.js
@@ -4,13 +4,13 @@ import ".//corporate.css";
 export default function CorporateCard(props) {
   return (
     <>
-      <div key={props.id} className="corporate-cards">
+      <div key={props.id} className="corporate-card">
         <div className="corporate-card-img">
-          <img src={props.image} width={500} />
+          <img src={props.image} width={450} />
         </div>
         <div className="corporate-info">
           <h2 className="corporate-heading"> {props.heading}</h2>
-          <p className="corporate-para">{props.paragraph}</p>
+          <p className="text corporate-para">{props.paragraph}</p>
         </div>
       </div>
     </>
